f404dd597a696a63b1899bf656f6b1498eea45d3
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2025-04-27 23:27:27 +0600

    feat: add message input and display functionality with socket.io

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
+io.on("connection", (socket) => {
+	console.log("a user connected");
+	socket.on("chat", (message) => {
+		console.log(message);
+	});
+});
 // Server setup
 server.listen(4000, () => {
 	console.log(`Server Running at http://localhost:4000`);
